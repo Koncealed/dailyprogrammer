15defced88b12f88b28953549bc6ebcf2231c5bb
Koncealed <robbieskonieczny@gmail.com>
2016-11-07 15:47:29 -0500

    DailyProgrammer#291

---
+++ DailyProgrammer#291.java
@@ -0,0 +1,37 @@
+class GoldilocksChecker {
+	public static void checkSeat(int[][] input){
+		int maxWeight = input[0][0];
+		int porridgeTemp = input[0][1];
+		String suitableChairs = "";
+		for(int i = 1; i < input.length;++i) {
+			int checkingWeight = input[i][0];
+			int checkingTemp = input[i][1];
+			if(checkingWeight > maxWeight && porridgeTemp > checkingTemp){
+				suitableChairs += i + " ";
+			}
+		}
+		if(suitableChairs == "") {
+			System.out.println("Tell dat bitch to lose some weight there are 0 chairs for her.");
+		} else {
+			System.out.println(suitableChairs);
+		}
+	} 
+}
+
+public class Goldilocks {
+
+	public static void main(String[] args) {
+		int[][] input = {  { 100, 80 },
+		                   { 30, 50 },
+				           { 130, 75 },
+				           { 90, 60 },
+				           { 150, 85 },
+				           { 120, 70 },
+				           { 200, 200 },
+				           { 110, 100 } };
+		System.out.println("Below is the Sample Input.");
+		GoldilocksChecker.checkSeat(input);//Sample input
+	}
+}
+
+
